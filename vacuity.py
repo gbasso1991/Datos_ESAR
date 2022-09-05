4f11b6c22eb814e58eb9060095ec9669eaae9d53
Giuliano <83291158+gbasso1991@users.noreply.github.com>
2022-09-05 20:34:39 -0300

    Avances de finde en Windorrr

--- vacuity.py
+++ vacuity.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 vacuity.py
-Created on Tue Aug 30 15:46:23 2022
+Created on Tue Aug 30 2022
 
 @author: Giuliano
 
@@ -32,8 +32,8 @@ import fnmatch
 import tkinter as tk
 from tkinter import filedialog
 from datetime import datetime
-
-#%% Seleccion Archivos muestra
+from scipy.interpolate import BSpline
+#% Seleccion Archivos muestra
 root = tk.Tk()
 root.withdraw()
 texto_encabezado = "Seleccionar archivos con las medidas de muestra:"
@@ -48,7 +48,7 @@ print('Directorio de trabajo: '+ directorio +'\n')
 print('Archivos de muestra: ')
 for item in fnames_m:
     print(' >',item)
-
+#%%
 '''
 Parámetros de la medida a partir de nombre del archivo de muestra: 'xxxkHz_yyydA_zzzMss_*.txt
 '''
@@ -63,33 +63,105 @@ for i in range(len(fnames_m)):
     samples_per_s.append(1e-6/float(fnames_m[i].split('_')[2][:-3]))
     fecha_m.append(datetime.fromtimestamp(os.path.getmtime(path_m[i])))
 
-delta_t_m = []
+delta_t_m = [] #seg
 for elem in fecha_m:
-    delta_t_m.append((elem - fecha_m[0]).total_seconds())
+    delta_t_m.append(int((elem - fecha_m[0]).total_seconds()))
 
 # #%% En windows, me conviene usar mtime, despues de todo, busco un timedelta
 # datetime.fromtimestamp(os.stat(path_m[0]).st_ctime).strftime('%F %H:%M:%S')
 # #%% 
 # datetime.fromtimestamp(os.stat(path_m[0]).st_mtime).strftime('%F %H:%M:%S')
 
-#%% Ahora levanto el log de temperaturas en .csv
-#directorio= 
-data = pd.read_csv('agua/220830_templog_01.csv',sep=';',header=5,names=('Timestamp','Temperatura'),usecols=(0,1),decimal=',',dtype = {'Timestamp': 'str', 'Temperatura': 'float'},parse_dates=['Timestamp'],engine='python') 
-temperatura = pd.Series(data['Temperatura'])
-temperatura = temperatura.to_numpy()
+#% Ahora levanto el log de temperaturas en .csv
 
-#= pd.read_csv('paramagneto/220830_templog_00.csv',sep=';',header=5,names=('Timestamp','Temperatura'),usecols=(0,1),decimal=',',parse_dates=['Timestamp'],engine='python') 
+directorio= 'paramagneto/220830_templog_00.csv' #automatizar esto
+#directorio= 'agua/220830_templog_01.csv' #automatizar esto
 
-timestamp = pd.Series(data['Timestamp'])
+data = pd.read_csv(directorio,sep=';',header=5,
+                    names=('Timestamp','Temperatura'),usecols=(0,1),
+                    decimal=',',engine='python') 
+temperatura = pd.Series(data['Temperatura']).to_numpy(dtype=float)
 
-timestamp = timestamp.to_numpy(dtype=datetime)
+timestamp=[]
+for time in pd.Series(data['Timestamp']):
+    timestamp.append(time[11:19])
+timestamp=np.array(timestamp,dtype=str)
 
-for e in timestamp:
-    e = datetime.strptime(e[11:19],'%H:%M:%S')
-    #print((e,)
+#% Datetime de las medidas de muestra en funcion del horario del 1er dato
+
+date_primer_dato = datetime(year=2022,month=8,day=30,
+                            hour=10,minute=39,second=59) #queda automatizar esto 
+#para agua, cambiar por 11:07:11 
+
+
+time_m = [] 
+for elem in delta_t_m:
+    time_m.append((date_primer_dato + timedelta(0,elem)).strftime('%H:%M:%S'))
+time_m = np.array(time_m) #H:M:S del registro de cada archivo muestra
+
+#obtengo los indices de estos horarios en el timestamp
+
+temp_m = []
+indx_temp = []
+for t in time_m:
+    indx_temp.append(np.flatnonzero(timestamp==t)[0])
+    temp_m.append(temperatura[np.flatnonzero(timestamp==t)[0]])
+temp_m=np.array(temp_m)
+#% Printeo lo obtenido
+print('Archivos de muestra: ')
+for i, item in enumerate(fnames_m):
+    print(item[-8:-4],' ',str(temp_m[i]) + ' ºC')
+
+
+
+
+
+
+#%% dif temporal en s entre el comienzo del registro y la primer medida de muestra
+#delta_0 = (datetime.strptime(time_m[0],'%H:%M:%S') - datetime.strptime(timestamp[0],'%H:%M:%S')).total_seconds()
+
 #%%
+# timestamp = pd.to_datetime(pd.Series(data['Timestamp']).to_numpy())
+# aux=[]
+# for element in timestamp:
+#     aux.append(element.strftime('%H:%M:%S'))
 
-
-
-
+#for e in timestamp:
+ #   e = datetime.strptime(e[11:19],'%H:%M:%S')
+    #print((e,)
 #plt.xticks(rotation=45, ha='right')
+#%% Ploteo los ciclos y asocio la temperatura
+#traigo los datos desde possessor
+
+Ciclos_eje_H=Ciclos_eje_H
+Ciclos_eje_M=Ciclos_eje_M
+Ciclos_eje_H_cal=Ciclos_eje_H_cal
+Ciclos_eje_M_cal_ua=Ciclos_eje_M_cal_ua
+SAR=SAR
+
+fig = plt.figure(figsize=(10,8),constrained_layout=True)
+ax = fig.add_subplot(1,1,1)
+axin = ax.inset_axes([0.60,0.08, 0.35,0.38])
+axin.set_title('Calibración',loc='center')
+#axin.yaxis.tick_right()
+plt.setp(axin.get_yticklabels(),visible=True)
+plt.setp(axin.get_xticklabels(),visible=True)
+axin.yaxis.tick_right()
+axin.grid()
+axin.axhline(0,0,1,lw=0.9,c='k')
+axin.axvline(0,0,1,lw=0.9,c='k')
+
+for i in range(len(fnames_m)):      
+    plt.plot(Ciclos_eje_H[i],Ciclos_eje_M[i],label=f'{fnames_m[i][-8:-4]}   {temp_m[i]}ºC')
+    axin.plot(Ciclos_eje_H_cal[i], Ciclos_eje_M_cal_ua[i])
+    axin.set_ylabel('M $(V\cdot s)$')
+    axin.set_xlabel('H $(A/m)$')
+
+plt.legend(loc='best',fancybox=True)
+plt.grid()
+plt.xlabel('Campo (A/m)',fontsize=15)
+plt.ylabel('Magnetización (A/m)',fontsize=15)
+plt.suptitle('Ciclos de histéresis en descongelamiento',fontsize=30)
+#plt.savefig('Ciclos_histeresis_descong_bis.png',dpi=300,facecolor='w')
+
+# %%
